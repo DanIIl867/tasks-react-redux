26a81ffe3f74e7d3ab7a3a94cb2f42400b82e936
Daniil <negativdaniil4@gmail.com>
2024-08-19 12:58:15 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,7 +3,13 @@
 // export const isError = state => state.tasks.error;
 // export const getFilters = state => state.filters.status;
 
+import { createSelectorHook } from "react-redux"
+
 export const isLoading = state => state.tasks.isLoading
 export const IsError = state => state.tasks.error
 export const getTasks = state => state.tasks.items
 export const getFilters = state => state.filter
+
+export const selectVisibleTasks = createSelector([selectTasks, selectFiltres],
+    (tasks, statusFilter) => 
+)
